11685b7cb883b521473dc52bfe2b9e53a4432709
Brian Zalewski <59970525+ProfessorManhattan@users.noreply.github.com>
2024-01-02 11:48:39 -0500

    Update ceph-client.rb

--- ceph-client.rb
+++ ceph-client.rb
@@ -10,7 +10,7 @@ class CephClient < Formula
   depends_on "openssl" => :build
   depends_on "cmake" => :build
   depends_on "ninja" => :build
-  depends_on "cython"
+  # depends_on "cython"
   depends_on "leveldb" => :build
   depends_on "nss"
   depends_on "pkg-config" => :build
@@ -50,7 +50,7 @@ class CephClient < Formula
     ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["openssl"].opt_lib}/pkgconfig"
     ENV.prepend_path "PKG_CONFIG_PATH", "/usr/local/lib/pkgconfig"
     xy = Language::Python.major_minor_version "python3"
-    ENV.prepend_create_path "PYTHONPATH", "#{Formula["cython"].opt_libexec}/lib/python3.12/site-packages"
+    # ENV.prepend_create_path "PYTHONPATH", "#{Formula["cython"].opt_libexec}/lib/python3.12/site-packages"
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
     resources.each do |resource|
       resource.stage do
